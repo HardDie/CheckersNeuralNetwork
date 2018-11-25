bce27c1dc5c36a031ba35532b83527f2969329c2
Oleg Sysoev <oleg.sysoev@eltex.loc>
2018-11-25 23:00:10 +0700

    Create pool of steps class

---
+++ pool.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <algorithm>
+#include <random>
+#include "pool.h"
+
+static std::random_device random_device;
+static std::mt19937 engine(random_device());
+
+Pool::Pool() :
+	returnedValue_(-1)
+{
+}
+
+Pool::~Pool() {
+	if (returnedValue_ != -1) {
+		std::cerr << __FUNCTION__
+		          << "(): Remove pool without save progress!\n";
+	}
+}
+
+bool Pool::AddStep(int value) {
+	if (value < 0 || value > 8) {
+		std::cerr << __FUNCTION__
+		          << "(): Invalid step!\n";
+		return false;
+	}
+
+	stepsPool_.push_back(value);
+	return true;
+}
+
+bool Pool::WasGoodStep(void) {
+	if (returnedValue_ == -1) {
+		std::cerr << __FUNCTION__
+		          << "(): Try save empty value!\n";
+		return false;
+	}
+
+	this->AddStep(returnedValue_);
+	returnedValue_ = -1;
+	return true;
+}
+
+bool Pool::WasBadStep(void) {
+	if (returnedValue_ == -1) {
+		std::cerr << __FUNCTION__
+		          << "(): Try save empty value!\n";
+		return false;
+	}
+
+	returnedValue_ = -1;
+	return true;
+}
+
+void Pool::Sort(void) {
+	std::sort(stepsPool_.begin(), stepsPool_.end());
+}
+
+int Pool::GetStep(void) {
+	std::uniform_int_distribution<int> dist(0, stepsPool_.size() - 1);
+	int randValue = dist(engine);
+	returnedValue_ = stepsPool_[randValue];
+	return returnedValue_;
+}
+
+void Pool::Print(void) const {
+	size_t len = stepsPool_.size();
+	for (size_t i = 0; i < len; i++) {
+		std::cout << stepsPool_[i] << " ";
+	}
+	std::cout << std::endl;
+}
---
+++ pool.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+class Pool {
+ public:
+	Pool();
+	~Pool();
+
+	bool AddStep(int value);
+	bool WasGoodStep(void);
+	bool WasBadStep(void);
+	void Sort(void);
+	int GetStep(void);
+
+	void Print(void) const;
+ private:
+	std::vector<int> stepsPool_;
+	int returnedValue_;
+};
